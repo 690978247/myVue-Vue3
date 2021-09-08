1ddb0b57516ac97e2c3bab7413b6033ee221b37c
‘xu’ <laike.xu@shengyc.com>
2021-09-08 17:05:23 +0800

    add vue-router 4.x

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,51 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, watch, computed } from 'vue'
 
 defineProps({
   msg: String
 })
 
 const count = ref(0)
+const list = [1,2,3,4,5]
+const itemRefs = []
+const calaulate = computed(() => count.value * 2)
+const theme = {
+  color: 'green'
+}
+// const color = ref('blue')
+const root = ref(null)
+
+const setItemRef = el => {
+  if (el) {
+    itemRefs.push(el)
+  }
+}
+watch(count, (newVal, oldVal) => {
+  console.log('The new counter value is: ' + count.value)
+})
+
+onMounted(() => {
+  setItemRef
+  console.log(root,root.value)
+})
 
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div ref="root">This is a root element</div>
+  <h1 class="text" >{{ msg }}</h1>
   <el-input v-model="count" ></el-input>
+  <el-input v-model="calaulate" ></el-input>
+  <div v-for="item in list" :key="item" :ref="setItemRef">{{item}}</div>
   <button type="button" @click="count++">count is: {{ count }}</button>
 </template>
 
 <style scoped>
 a {
   color: #42b983;
+}
+.text {
+  color: v-bind('theme.color');
+  /* color: v-bind('color'); */
 }
 </style>
