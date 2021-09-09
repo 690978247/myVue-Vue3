d2bab363de5567d722c59e9dee138fa8812c9492
‘xu’ <laike.xu@shengyc.com>
2021-09-09 11:17:27 +0800

    fix bug

---
+++ src/components/404NotFound.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h5>This is 404 Not Found Page</h5>  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,27 @@
 import { createRouter, createWebHashHistory  } from "vue-router"
-import Helloword from '../components/HelloWorld.vue'
+import NotFound from '../components/404NotFound.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(), // hash模式：createWebHashHistory，history模式：createWebHistory
+  history: createWebHashHistory(), // hash模式：createWebHashHistory，history模式：createWebHistory,abstract模式: createMemoryHistory()
   routes:[
-      {
-        path: '/',
-        component: Helloword
+    {
+      path: '/',
+      component: () => import('../components/HelloWorld.vue')
+    },
+    {
+      path: '/helloword',
+      component: () => import('../components/HelloWorld.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound
     }
   ]
 })
+
+// 命名路由
+// router.resolve({
+//   name: 'not-found',
+//   params: { pathMatch:['not', 'found']}
+// }).href
 
 export default router
