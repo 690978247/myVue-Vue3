97342cf91db2219faa66e2f2966e0704ec06c910
‘xu’ <laike.xu@shengyc.com>
2021-09-09 16:14:07 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -34,49 +34,49 @@ const router = createRouter({
         {
           path: 'nav1-1',
           name: 'Nav1-1',
-          component: () => import('@/views/navPage/nav1/nav1-1.vue'),
+          component: () => import('../views/navPage/nav1/nav1-1.vue'),
           meta: { title: 'nav1-1页面' }
         },
         {
           path: 'nav1-2',
           name: 'Nav1-2',
-          component: () => import('@/views/navPage/nav1/nav1-2.vue'),
+          component: () => import('../views/navPage/nav1/nav1-2.vue'),
           meta: { title: 'nav1-2页面' }
         },
         {
           path: 'nav1-3',
           name: 'Nav1-3',
-          component: () => import('@/views/navPage/nav1/nav1-3.vue'),
+          component: () => import('../views/navPage/nav1/nav1-3.vue'),
           meta: { title: 'nav1-3页面' }
         },
         {
           path: 'nav1-4',
           name: 'Nav1-4',
-          component: () => import('@/views/navPage/nav1/nav1-4-1.vue'),
+          component: () => import('../views/navPage/nav1/nav1-4-1.vue'),
           meta: { title: 'nav1-4页面' }
         },
         {
           path: 'nav2-1',
           name: 'Nav2-1',
-          component: () => import('@/views/navPage/nav2/nav2-1.vue'),
+          component: () => import('../views/navPage/nav2/nav2-1.vue'),
           meta: { title: 'nav2-1页面' }
         },
         {
           path: 'nav2-2',
           name: 'Nav2-2',
-          component: () => import('@/views/navPage/nav2/nav2-2-1.vue'),
+          component: () => import('../views/navPage/nav2/nav2-2-1.vue'),
           meta: { title: 'nav2-2页面' }
         },
         {
           path: 'nav3-1',
           name: 'Nav3-1',
-          component: () => import('@/views/navPage/nav3/nav3-1.vue'),
+          component: () => import('../views/navPage/nav3/nav3-1.vue'),
           meta: { title: 'nav3-1页面' }
         },
         {
           path: 'nav4-1',
           name: 'Nav4-1',
-          component: () => import('@/views/navPage/nav4/nav4-1.vue'),
+          component: () => import('../views/navPage/nav4/nav4-1.vue'),
           meta: { title: 'nav4-1页面' }
         }
       ]
