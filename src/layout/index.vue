ba22d3d20404eabcd8ec3ff03169bb5774572ccf
‘xu’ <laike.xu@shengyc.com>
2021-10-21 09:54:24 +0800

    fix bug

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -26,22 +26,27 @@ import sideBar from './components/sidebar.vue'
     min-width: $aside-width;
     height: 100%;
     background: #545C64;
+    transition: all .5s;
     color: #fff;
+    overflow: hidden;
   }
   .app-main {
     width: 100%;
     height: 100%;
-    padding: 10px;
-    box-sizing: border-box;
+    background: #f0f0f0;
     .main-header{
       width: 100%;
       height: 65px;
+      background: #545C64;
       display: flex;
       align-items: center;
+      color: #fff
     }
     .main-wrap {
       width: 100%;
-      height: calc(100% - 65px)
+      padding: 10px;
+      height: calc(100% - 65px);
+      box-sizing: border-box;
     }
   }
   
