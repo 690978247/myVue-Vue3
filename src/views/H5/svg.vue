69d83020918fafb5b13ce9e9eefbc88f5f54c4ca
‘xu’ <laike.xu@shengyc.com>
2021-11-30 17:00:16 +0800

    add echarts

--- src/views/H5/svg.vue
+++ src/views/H5/svg.vue
@@ -1,15 +1,77 @@
 <template>
   <div>
-    这是一个SVG页面
+    <svg class="svg" >
+      <circle v-for="(item, index) in dots" :key="index" :cx="item.x" :cy="item.y" :r="item.r" :stroke="item.color" stroke-width="1" :fill="item.color" />
+
+      <!-- 高斯模糊 filter链接id-->
+      <!-- <defs>
+        <filter id="f1" x="0" y="0">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="15" />
+        </filter>
+      </defs>
+      <rect width="90" height="90" x="150" stroke="green" stroke-width="3" fill="yellow" filter="url(#f1)" /> -->
+
+      <!-- feOffset -->
+      <!-- <defs>
+        <filter id="f2" x="0" y="0" width="200%" height="200%" >
+          <feOffset result="offOut" in="SourceGraphic" dx="20" dy="20" />
+          <feBlend in="SourceGraphic" in2="offOut" mode="normal" />
+        </filter>
+      </defs>
+      <rect width="90" height="90" stroke="green" stroke-width="3" fill="yellow" filter="url(#f2)" /> -->
+
+    </svg>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SVG'
+  name: 'SVG',
+  data () {
+    return {
+      dots: [
+        {
+          x: 180,
+          y: 300,
+          r: 4,
+          color: '#D9001B',
+        },
+        {
+          x: 220,
+          y: 310,
+          r: 4,
+          color: '#FFFF00',
+        },
+        {
+          x: 60,
+          y: 130,
+          r: 4,
+          color: '#D9001B',
+        },
+        {
+          x: 160,
+          y: 380,
+          r: 4,
+          color: '#D9001B',
+        },
+        {
+          x: 250,
+          y: 150,
+          r: 4,
+          color: '#FFFF00'
+        }
+      ]
+    }
+  },
 }
 </script>
 
 <style>
-
+.svg {
+  width: 800px;
+  height: 500px;
+  border: 1px solid #ccc;
+  background: url('../../assets/404_images/404.png') no-repeat;
+  background-size: 100%;
+}
 </style>
--- src/views/navPage/nav4/nav4-1.vue
+++ src/views/navPage/nav4/nav4-1.vue
@@ -1,13 +1,76 @@
 <template>
   <div>
-    这是nav4-1页面
+    <el-button @click="print" >打印</el-button>
+    <!--startprint-->
+      <div id="printContent">
+        <img src="../../../assets/logo.png" alt="">
+      </div>
+      <div ref="myChart" id="main" style="width: 600px;height:400px;"></div>
+    <!--endprint-->
+
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Nav4-1'
+<script setup>
+import * as echarts from 'echarts';
+import { ref, onMounted } from 'vue'
+// export default {
+//   name: 'Nav4-1',
+//   data () {
+//     return {
+//       echart: ''
+//     }
+//   },
+//   methods: {
+//     print() {
+//       // console.log(window.document)
+//       let oldStr = window.document.body.innerHTML
+//       let start = "<!--startprint-->" // 开始打印标识, 17个字符
+//       let end = "<!--endprint-->" // 结束打印标识
+//       let newStr = oldStr.substr(oldStr.indexOf(start) + 17) // 截取开始打印标识之后的内容
+//       newStr = newStr.substring(0, newStr.indexOf(end)) 
+//       window.document.body.innerHTML = newStr;
+//       window.print()
+//       window.document.body.innerHTML = oldStr;
+//     },
+//   }
+// }
+let myChart = ref('')
+
+onMounted(() => {
+  initChart()
+})
+
+const initChart = () => {
+  myChart = echarts.init(document.getElementById("main"));
+  myChart.setOption({
+      title: { text: "总用户量" },
+      tooltip: {},
+      xAxis: {
+        data: ["12-3", "12-4", "12-5", "12-6", "12-7", "12-8"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "用户量",
+          type: "line",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    })
 }
+
+const print = () => {
+  let oldStr = window.document.body.innerHTML
+  let start = "<!--startprint-->" // 开始打印标识, 17个字符
+  let end = "<!--endprint-->" // 结束打印标识
+  let newStr = oldStr.substr(oldStr.indexOf(start) + 17) // 截取开始打印标识之后的内容
+  newStr = newStr.substring(0, newStr.indexOf(end)) 
+  window.document.body.innerHTML = newStr;
+  window.print()
+  window.document.body.innerHTML = oldStr;
+}
+
 </script>
 
 <style lang="scss" scoped>
